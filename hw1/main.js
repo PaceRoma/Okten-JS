cb866ea570db90033534fb8a16c498153d163563
PaceRoma <roman.shchepan@gmail.com>
2022-10-31 23:37:13 +0200

    hw1

---
+++ hw1/main.js
@@ -0,0 +1,39 @@
+"use strict"
+
+let s1 = 'hello';
+console.log(s1);
+let s2 = 'owu';
+console.log(s2);
+let s3 = 'com';
+console.log(s3);
+let s4 = 'ua';
+console.log(s4);
+///
+let num1 = 1
+console.log(num1)
+let num2 = 10
+console.log(num2)
+let num3 = -999
+console.log(num3)
+let num4 = 123
+console.log(num4)
+let num5 = 3.14
+console.log(num5)
+let num6 = 2.7
+console.log(num6)
+///
+console.log(5<4)
+console.log(4===4)
+///
+let fName = "sergey"
+let mName = "sergeyovich"
+let lName = "sergov"
+console.log(fName, mName, lName)
+
+let a = 100;
+console.log(typeof (a))
+let b = '100';
+console.log(typeof (b))
+let c = true;
+console.log(typeof (c))
+
